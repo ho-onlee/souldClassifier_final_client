266240118617650842b2272af1c5afae24a14e4f
Hoon <hoonnhun711@gmail.com>
2026-04-03 16:54:21 -0400

    modified:   root.py

--- root.py
+++ root.py
@@ -1,5 +1,16 @@
-from sound_to_tensor import *
+import src.record_audio as record_audio
+import src.decibel_meter as decibel_meter
 
+def callback(frames):
+    print(f"\nSaved {len(frames)} frames to WAV file.")
+    print(f"\ndb Level: {meter.read_decibel()} dB SPL")
 
-
-features = torch.tensor(features, dtype=torch.float32)
+if __name__ == "__main__":
+    recorder = record_audio.AudioRecorder(
+        output_path="Recordings",
+        duration_seconds=10,
+        source_index="2"
+    )
+    meter = decibel_meter.DecibelMeter()
+    recorder.setCallback(callback)
+    recorder.start_stream()
